93d4326c3b46e515529c1956891a5b04fd00322c
doris40421-cloud <doris40421@gmail.com>
2026-06-28 21:37:51 +0800

    Change app.run to use localhost and port 5000

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # debug=True 代表改程式碼網頁會自動重啟，方便開發
-    app.run(debug=True, port=000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
